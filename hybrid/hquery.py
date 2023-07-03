9992aaba360d451b5bd42a19ae499d8b2c3665a6
Scott Soderstrom <ssoderstrom@gmail.com>
2023-07-02 18:31:14 -1000

    aider: Set `OPENAI_API_KEY` environment variable using `os.environ` and use it when initializing `OpenAIEmbeddings` and `OpenAI` objects.
    # Context:
    USER: please add an environment variable `OPENAI_API_KEY` =  "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" using thr correct syntax
    ASSISTANT: To add an environment variable in Python, you can use the `os` module's `environ` object. Here's how you can set the `OPENAI_API_KEY` environment variable in your `hybrid/hquery.py` file:
    
    ```python
    hybrid/hquery.py
    <<<<<<< ORIGINAL
    embed = OpenAIEmbeddings(
        model=model_name,
        openai_api_key="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"
    )
    llm=OpenAI(model_name="text-embedding-ada-002")
    from getpass import getpass
    OPENAI_API_KEY = getpass("OpenAI API Key: ")  # platform.openai.com
    =======
    import os
    os.environ['OPENAI_API_KEY'] = "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"
    embed = OpenAIEmbeddings(
        model=model_name,
        openai_api_key=os.getenv('OPENAI_API_KEY')
    )
    llm=OpenAI(model_name="text-embedding-ada-002")
    >>>>>>> UPDATED
    ```
    
    This change sets the `OPENAI_API_KEY` environment variable and then uses it when initializing the `OpenAIEmbeddings` and `OpenAI` objects.

--- hybrid/hquery.py
+++ hybrid/hquery.py
@@ -12,13 +12,13 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 print(os.getenv('OPENAI_API_KEY'))
 model_name = 'text-embedding-ada-002'
 
+import os
+os.environ['OPENAI_API_KEY'] = "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"
 embed = OpenAIEmbeddings(
     model=model_name,
-    openai_api_key="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"
+    openai_api_key=os.getenv('OPENAI_API_KEY')
 )
 llm=OpenAI(model_name="text-embedding-ada-002")
-from getpass import getpass
-OPENAI_API_KEY = getpass("OpenAI API Key: ")  # platform.openai.com
 print("Imported necessary libraries")
 
 # init connection to pinecone
